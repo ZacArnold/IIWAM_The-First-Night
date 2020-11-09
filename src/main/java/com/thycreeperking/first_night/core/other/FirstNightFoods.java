07adfb93a87eb997ee76c433d99a48f9e29b7096
Thy Creeper King <67208693+ThyCreeperKing@users.noreply.github.com>
2020-11-09 10:09:40 -0800

    Update FirstNightFoods.java

--- src/main/java/com/thycreeperking/first_night/core/other/FirstNightFoods.java
+++ src/main/java/com/thycreeperking/first_night/core/other/FirstNightFoods.java
@@ -7,6 +7,6 @@ import net.minecraftforge.fml.common.Mod;
 @Mod.EventBusSubscriber(modid = FirstNight.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class FirstNightFoods {
 
-    public static final Food ROASTED_CARROT = new Food.Builder().hunger(6).saturation(5.0F).build();
+    public static final Food ROASTED_CARROT = new Food.Builder().hunger(6).saturation(0.5F).build();
 
 }
