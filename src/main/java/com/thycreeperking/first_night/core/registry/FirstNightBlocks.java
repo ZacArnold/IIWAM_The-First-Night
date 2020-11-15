a06d7356eb4f8d081cbf70113ab607bf74b4b312
Thy Creeper King <67208693+ThyCreeperKing@users.noreply.github.com>
2020-11-14 17:01:54 -0800

    Cobble & Mossy Cobble Bricks Registry

--- src/main/java/com/thycreeperking/first_night/core/registry/FirstNightBlocks.java
+++ src/main/java/com/thycreeperking/first_night/core/registry/FirstNightBlocks.java
@@ -12,7 +12,9 @@ public class FirstNightBlocks {
 
     public static final RegistryHelper HELPER = FirstNight.REGISTRY_HELPER;
 
-    ////FIRST NIGHT BLOCKS////
+
+    ////////////////////////////////////////////////////////////FIRST NIGHT BLOCKS////////////////////////////////////////////////////////////
+    //Cobblestone Blocks//
     //Andesite Cobble Block Set
     public static final RegistryObject<Block> ANDESITE_COBBLESTONE = HELPER.createBlock("andesite_cobblestone", () ->
             new Block(Block.Properties.from(Blocks.COBBLESTONE)), FirstNight.FIRST_NIGHT_TAB);
@@ -73,7 +75,7 @@ public class FirstNightBlocks {
 
 
 
-    ////COMPAT BLOCKS////
+    ////////////////////////////////////////////////////////////COMPAT BLOCKS////////////////////////////////////////////////////////////
     //First Night//
     //Andesite Cobble Bricks Block Set
     public static final RegistryObject<Block> ANDESITE_COBBLESTONE_BRICKS = HELPER.createCompatBlock("quark", "andesite_cobblestone_bricks", () ->
@@ -112,7 +114,6 @@ public class FirstNightBlocks {
             new StairsBlock(MOSSY_DIORITE_COBBLESTONE_BRICKS.get().getDefaultState(), Block.Properties.from(Blocks.COBBLESTONE_STAIRS)), FirstNight.FIRST_NIGHT_TAB);
     public static final RegistryObject<Block> MOSSY_DIORITE_COBBLESTONE_BRICKS_WALL = HELPER.createCompatBlock("quark","mossy_diorite_cobblestone_bricks_wall", () ->
             new WallBlock(Block.Properties.from(Blocks.COBBLESTONE_WALL)), FirstNight.FIRST_NIGHT_TAB);
-
 
     //Granite Cobble Bricks Block Set
     public static final RegistryObject<Block> GRANITE_COBBLESTONE_BRICKS = HELPER.createCompatBlock("quark", "granite_cobblestone_bricks", () ->
@@ -154,9 +155,23 @@ public class FirstNightBlocks {
     public static final RegistryObject<Block> CHORUS_BASALT_COBBLESTONE_WALL = HELPER.createCompatBlock("quark","chorus_basalt_cobblestone_wall", () ->
             new WallBlock(Block.Properties.from(Blocks.COBBLESTONE_WALL)), FirstNight.FIRST_NIGHT_TAB);
     //Voidstone Cobble Bricks Block Set
-
+    public static final RegistryObject<Block> BASALT_COBBLESTONE_BRICKS = HELPER.createCompatBlock("quark", "basalt_cobblestone_bricks", () ->
+            new Block(Block.Properties.from(Blocks.COBBLESTONE)), FirstNight.FIRST_NIGHT_TAB);
+    public static final RegistryObject<Block> BASALT_COBBLESTONE_BRICKS_SLAB = HELPER.createCompatBlock("quark","basalt_cobblestone_bricks_slab", () ->
+            new SlabBlock(Block.Properties.from(Blocks.COBBLESTONE_SLAB)), FirstNight.FIRST_NIGHT_TAB);
+    public static final RegistryObject<Block> BASALT_COBBLESTONE_BRICKS_STAIRS = HELPER.createCompatBlock("quark","basalt_cobblestone_bricks_stairs", () ->
+            new StairsBlock(BASALT_COBBLESTONE_BRICKS.get().getDefaultState(), Block.Properties.from(Blocks.COBBLESTONE_STAIRS)), FirstNight.FIRST_NIGHT_TAB);
+    public static final RegistryObject<Block> BASALT_COBBLESTONE_BRICKS_WALL = HELPER.createCompatBlock("quark","basalt_cobblestone_bricks_wall", () ->
+            new WallBlock(Block.Properties.from(Blocks.COBBLESTONE_WALL)), FirstNight.FIRST_NIGHT_TAB);
     //Chorus Voidstone Cobble Bricks Block Set
-
+    public static final RegistryObject<Block> CHORUS_BASALT_COBBLESTONE_BRICKS = HELPER.createCompatBlock("quark", "chorus_basalt_cobblestone_bricks", () ->
+            new Block(Block.Properties.from(Blocks.COBBLESTONE)), FirstNight.FIRST_NIGHT_TAB);
+    public static final RegistryObject<Block> CHORUS_BASALT_COBBLESTONE_BRICKS_SLAB = HELPER.createCompatBlock("quark","chorus_basalt_cobblestone_bricks_slab", () ->
+            new SlabBlock(Block.Properties.from(Blocks.COBBLESTONE_SLAB)), FirstNight.FIRST_NIGHT_TAB);
+    public static final RegistryObject<Block> CHORUS_BASALT_COBBLESTONE_BRICKS_STAIRS = HELPER.createCompatBlock("quark","chorus_basalt_cobblestone_bricks_stairs", () ->
+            new StairsBlock(CHORUS_BASALT_COBBLESTONE_BRICKS.get().getDefaultState(), Block.Properties.from(Blocks.COBBLESTONE_STAIRS)), FirstNight.FIRST_NIGHT_TAB);
+    public static final RegistryObject<Block> CHORUS_BASALT_COBBLESTONE_BRICKS_WALL = HELPER.createCompatBlock("quark","chorus_basalt_cobblestone_bricks_wall", () ->
+            new WallBlock(Block.Properties.from(Blocks.COBBLESTONE_WALL)), FirstNight.FIRST_NIGHT_TAB);
 
     //Jasper Cobble Block Set
     public static final RegistryObject<Block> JASPER_COBBLESTONE = HELPER.createCompatBlock("quark", "jasper_cobblestone", () ->
@@ -177,9 +192,23 @@ public class FirstNightBlocks {
     public static final RegistryObject<Block> MOSSY_JASPER_COBBLESTONE_WALL = HELPER.createCompatBlock("quark","mossy_jasper_cobblestone_wall", () ->
             new WallBlock(Block.Properties.from(Blocks.COBBLESTONE_WALL)), FirstNight.FIRST_NIGHT_TAB);
     //Jasper Cobble Bricks Block Set
-
+    public static final RegistryObject<Block> JASPER_COBBLESTONE_BRICKS = HELPER.createCompatBlock("quark", "jasper_cobblestone_bricks", () ->
+            new Block(Block.Properties.from(Blocks.COBBLESTONE)), FirstNight.FIRST_NIGHT_TAB);
+    public static final RegistryObject<Block> JASPER_COBBLESTONE_SLAB_BRICKS = HELPER.createCompatBlock("quark","jasper_cobblestone_bricks_slab", () ->
+            new SlabBlock(Block.Properties.from(Blocks.COBBLESTONE_SLAB)), FirstNight.FIRST_NIGHT_TAB);
+    public static final RegistryObject<Block> JASPER_COBBLESTONE_STAIRS_BRICKS = HELPER.createCompatBlock("quark","jasper_cobblestone_bricks_stairs", () ->
+            new StairsBlock(JASPER_COBBLESTONE_BRICKS.get().getDefaultState(), Block.Properties.from(Blocks.COBBLESTONE_STAIRS)), FirstNight.FIRST_NIGHT_TAB);
+    public static final RegistryObject<Block> JASPER_COBBLESTONE_WALL_BRICKS = HELPER.createCompatBlock("quark","jasper_cobblestone_bricks_wall", () ->
+            new WallBlock(Block.Properties.from(Blocks.COBBLESTONE_WALL)), FirstNight.FIRST_NIGHT_TAB);
     //Mossy Jasper Cobble Bricks Block Set
-
+    public static final RegistryObject<Block> MOSSY_JASPER_COBBLESTONE_BRICKS = HELPER.createCompatBlock("quark", "mossy_jasper_cobblestone_bricks", () ->
+            new Block(Block.Properties.from(Blocks.COBBLESTONE)), FirstNight.FIRST_NIGHT_TAB);
+    public static final RegistryObject<Block> MOSSY_JASPER_COBBLESTONE_BRICKS_SLAB = HELPER.createCompatBlock("quark","mossy_jasper_cobblestone_bricks_slab", () ->
+            new SlabBlock(Block.Properties.from(Blocks.COBBLESTONE_SLAB)), FirstNight.FIRST_NIGHT_TAB);
+    public static final RegistryObject<Block> MOSSY_JASPER_COBBLESTONE_BRICKS_STAIRS = HELPER.createCompatBlock("quark","mossy_jasper_cobblestone_bricks_stairs", () ->
+            new StairsBlock(MOSSY_JASPER_COBBLESTONE_BRICKS.get().getDefaultState(), Block.Properties.from(Blocks.COBBLESTONE_STAIRS)), FirstNight.FIRST_NIGHT_TAB);
+    public static final RegistryObject<Block> MOSSY_JASPER_COBBLESTONE_BRICKS_WALL = HELPER.createCompatBlock("quark","mossy_jasper_cobblestone_bricks_wall", () ->
+            new WallBlock(Block.Properties.from(Blocks.COBBLESTONE_WALL)), FirstNight.FIRST_NIGHT_TAB);
 
     //Limestone Cobble Block Set
     public static final RegistryObject<Block> LIMESTONE_COBBLESTONE = HELPER.createCompatBlock("quark", "limestone_cobblestone", () ->
@@ -200,9 +229,23 @@ public class FirstNightBlocks {
     public static final RegistryObject<Block> MOSSY_LIMESTONE_COBBLESTONE_WALL = HELPER.createCompatBlock("quark","mossy_limestone_cobblestone_wall", () ->
             new WallBlock(Block.Properties.from(Blocks.COBBLESTONE_WALL)), FirstNight.FIRST_NIGHT_TAB);
     //Limestone Cobble Bricks Block Set
-
+    public static final RegistryObject<Block> LIMESTONE_COBBLESTONE_BRICKS = HELPER.createCompatBlock("quark", "limestone_cobblestone_bricks", () ->
+            new Block(Block.Properties.from(Blocks.COBBLESTONE)), FirstNight.FIRST_NIGHT_TAB);
+    public static final RegistryObject<Block> LIMESTONE_COBBLESTONE_BRICKS_SLAB = HELPER.createCompatBlock("quark","limestone_cobblestone_bricks_slab", () ->
+            new SlabBlock(Block.Properties.from(Blocks.COBBLESTONE_SLAB)), FirstNight.FIRST_NIGHT_TAB);
+    public static final RegistryObject<Block> LIMESTONE_COBBLESTONE_BRICKS_STAIRS = HELPER.createCompatBlock("quark","limestone_cobblestone_bricks_stairs", () ->
+            new StairsBlock(LIMESTONE_COBBLESTONE_BRICKS.get().getDefaultState(), Block.Properties.from(Blocks.COBBLESTONE_STAIRS)), FirstNight.FIRST_NIGHT_TAB);
+    public static final RegistryObject<Block> LIMESTONE_COBBLESTONE_BRICKS_WALL = HELPER.createCompatBlock("quark","limestone_cobblestone_bricks_wall", () ->
+            new WallBlock(Block.Properties.from(Blocks.COBBLESTONE_WALL)), FirstNight.FIRST_NIGHT_TAB);
     //Mossy Limestone Cobble Bricks Block Set
-
+    public static final RegistryObject<Block> MOSSY_LIMESTONE_COBBLESTONE_BRICKS = HELPER.createCompatBlock("quark", "mossy_limestone_cobblestone_bricks", () ->
+            new Block(Block.Properties.from(Blocks.COBBLESTONE)), FirstNight.FIRST_NIGHT_TAB);
+    public static final RegistryObject<Block> MOSSY_LIMESTONE_COBBLESTONE_BRICKS_SLAB = HELPER.createCompatBlock("quark","mossy_limestone_cobblestone_bricks_slab", () ->
+            new SlabBlock(Block.Properties.from(Blocks.COBBLESTONE_SLAB)), FirstNight.FIRST_NIGHT_TAB);
+    public static final RegistryObject<Block> MOSSY_LIMESTONE_COBBLESTONE_BRICKS_STAIRS = HELPER.createCompatBlock("quark","mossy_limestone_cobblestone_bricks_stairs", () ->
+            new StairsBlock(MOSSY_LIMESTONE_COBBLESTONE.get().getDefaultState(), Block.Properties.from(Blocks.COBBLESTONE_STAIRS)), FirstNight.FIRST_NIGHT_TAB);
+    public static final RegistryObject<Block> MOSSY_LIMESTONE_COBBLESTONE_BRICKS_WALL = HELPER.createCompatBlock("quark","mossy_limestone_cobblestone_bricks_wall", () ->
+            new WallBlock(Block.Properties.from(Blocks.COBBLESTONE_WALL)), FirstNight.FIRST_NIGHT_TAB);
 
     //Marble Cobble Block Set
     public static final RegistryObject<Block> MARBLE_COBBLESTONE = HELPER.createCompatBlock("quark", "marble_cobblestone", () ->
@@ -223,9 +266,23 @@ public class FirstNightBlocks {
     public static final RegistryObject<Block> MOSSY_MARBLE_COBBLESTONE_WALL = HELPER.createCompatBlock("quark","mossy_marble_cobblestone_wall", () ->
             new WallBlock(Block.Properties.from(Blocks.COBBLESTONE_WALL)), FirstNight.FIRST_NIGHT_TAB);
     //Marble Cobble Bricks Block Set
-
+    public static final RegistryObject<Block> MARBLE_COBBLESTONE_BRICKS = HELPER.createCompatBlock("quark", "marble_cobblestone_bricks", () ->
+            new Block(Block.Properties.from(Blocks.COBBLESTONE)), FirstNight.FIRST_NIGHT_TAB);
+    public static final RegistryObject<Block> MARBLE_COBBLESTONE_BRICKS_SLAB = HELPER.createCompatBlock("quark","marble_cobblestone_bricks_slab", () ->
+            new SlabBlock(Block.Properties.from(Blocks.COBBLESTONE_SLAB)), FirstNight.FIRST_NIGHT_TAB);
+    public static final RegistryObject<Block> MARBLE_COBBLESTONE_BRICKS_STAIRS = HELPER.createCompatBlock("quark","marble_cobblestone_bricks_stairs", () ->
+            new StairsBlock(MARBLE_COBBLESTONE_BRICKS.get().getDefaultState(), Block.Properties.from(Blocks.COBBLESTONE_STAIRS)), FirstNight.FIRST_NIGHT_TAB);
+    public static final RegistryObject<Block> MARBLE_COBBLESTONE_BRICKS_WALL = HELPER.createCompatBlock("quark","marble_cobblestone_bricks_wall", () ->
+            new WallBlock(Block.Properties.from(Blocks.COBBLESTONE_WALL)), FirstNight.FIRST_NIGHT_TAB);
     //Mossy Marble Cobble Bricks Block Set
-
+    public static final RegistryObject<Block> MOSSY_MARBLE_COBBLESTONE_BRICKS = HELPER.createCompatBlock("quark", "mossy_marble_cobblestone_bricks", () ->
+            new Block(Block.Properties.from(Blocks.COBBLESTONE)), FirstNight.FIRST_NIGHT_TAB);
+    public static final RegistryObject<Block> MOSSY_MARBLE_COBBLESTONE_BRICKS_SLAB = HELPER.createCompatBlock("quark","mossy_marble_cobblestone_bricks_slab", () ->
+            new SlabBlock(Block.Properties.from(Blocks.COBBLESTONE_SLAB)), FirstNight.FIRST_NIGHT_TAB);
+    public static final RegistryObject<Block> MOSSY_MARBLE_COBBLESTONE_BRICKS_STAIRS = HELPER.createCompatBlock("quark","mossy_marble_cobblestone_bricks_stairs", () ->
+            new StairsBlock(MOSSY_MARBLE_COBBLESTONE_BRICKS.get().getDefaultState(), Block.Properties.from(Blocks.COBBLESTONE_STAIRS)), FirstNight.FIRST_NIGHT_TAB);
+    public static final RegistryObject<Block> MOSSY_MARBLE_COBBLESTONE_BRICKS_WALL = HELPER.createCompatBlock("quark","mossy_marble_cobblestone_bricks_wall", () ->
+            new WallBlock(Block.Properties.from(Blocks.COBBLESTONE_WALL)), FirstNight.FIRST_NIGHT_TAB);
 
     //Slate Cobble Block Set
     public static final RegistryObject<Block> SLATE_COBBLESTONE = HELPER.createCompatBlock("quark", "slate_cobblestone", () ->
@@ -246,6 +303,21 @@ public class FirstNightBlocks {
     public static final RegistryObject<Block> ICY_SLATE_COBBLESTONE_WALL = HELPER.createCompatBlock("quark","icy_slate_cobblestone_wall", () ->
             new WallBlock(Block.Properties.from(Blocks.COBBLESTONE_WALL)), FirstNight.FIRST_NIGHT_TAB);
     //Slate Cobble Bricks Block Set
-
+    public static final RegistryObject<Block> SLATE_COBBLESTONE_BRICKS = HELPER.createCompatBlock("quark", "slate_cobblestone_bricks", () ->
+            new Block(Block.Properties.from(Blocks.COBBLESTONE)), FirstNight.FIRST_NIGHT_TAB);
+    public static final RegistryObject<Block> SLATE_COBBLESTONE_BRICKS_SLAB = HELPER.createCompatBlock("quark","slate_cobblestone_bricks_slab", () ->
+            new SlabBlock(Block.Properties.from(Blocks.COBBLESTONE_SLAB)), FirstNight.FIRST_NIGHT_TAB);
+    public static final RegistryObject<Block> SLATE_COBBLESTONE_BRICKS_STAIRS = HELPER.createCompatBlock("quark","slate_cobblestone_bricks_stairs", () ->
+            new StairsBlock(SLATE_COBBLESTONE_BRICKS.get().getDefaultState(), Block.Properties.from(Blocks.COBBLESTONE_STAIRS)), FirstNight.FIRST_NIGHT_TAB);
+    public static final RegistryObject<Block> SLATE_COBBLESTONE_BRICKS_WALL = HELPER.createCompatBlock("quark","slate_cobblestone_bricks_wall", () ->
+            new WallBlock(Block.Properties.from(Blocks.COBBLESTONE_WALL)), FirstNight.FIRST_NIGHT_TAB);
     //Icy Slate Cobble Bricks Block Set
+    public static final RegistryObject<Block> ICY_SLATE_COBBLESTONE_BRICKS = HELPER.createCompatBlock("quark", "icy_slate_cobblestone_bricks", () ->
+            new Block(Block.Properties.from(Blocks.COBBLESTONE)), FirstNight.FIRST_NIGHT_TAB);
+    public static final RegistryObject<Block> ICY_SLATE_COBBLESTONE_BRICKS_SLAB = HELPER.createCompatBlock("quark","icy_slate_cobblestone_bricks_slab", () ->
+            new SlabBlock(Block.Properties.from(Blocks.COBBLESTONE_SLAB)), FirstNight.FIRST_NIGHT_TAB);
+    public static final RegistryObject<Block> ICY_SLATE_COBBLESTONE_BRICKS_STAIRS = HELPER.createCompatBlock("quark","icy_slate_cobblestone_bricks_stairs", () ->
+            new StairsBlock(ICY_SLATE_COBBLESTONE_BRICKS.get().getDefaultState(), Block.Properties.from(Blocks.COBBLESTONE_STAIRS)), FirstNight.FIRST_NIGHT_TAB);
+    public static final RegistryObject<Block> ICY_SLATE_COBBLESTONE_BRICKS_WALL = HELPER.createCompatBlock("quark","icy_slate_cobblestone_bricks_wall", () ->
+            new WallBlock(Block.Properties.from(Blocks.COBBLESTONE_WALL)), FirstNight.FIRST_NIGHT_TAB);
 }
